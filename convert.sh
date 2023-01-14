e6f29e9f215a6308bcba8029cb39b165a17f2ac3
Ruben Garcia <rubengarciahernandez@gmail.com>
2023-01-14 16:41:06 +0100

    Fix submenus 3 -> v

--- convert.sh
+++ convert.sh
@@ -1,9 +1,12 @@
-sed -i -e  's=https://www.kerarqueo.com/=./=g#' index*
-sed -i -e 's=https://www.kerarqueo.com/=../=g#' */index*
-sed -i -e 's=https://www.kerarqueo.com/=../../=g#' */*/index*
-sed -i -e 's=https://www.kerarqueo.com/=../../../=g#' */*/*/index*
-sed -i -e 's=https://www.kerarqueo.com/=../../../../=g#' */*/*/*/index*
-sed -i -e 's=https://www.kerarqueo.com/=../../../../../=g#' */*/*/*/*/index*
-
-
+#relative links
+sed -i -e  's=https://www.kerarqueo.com/=./=g' index*
+sed -i -e 's=https://www.kerarqueo.com/=../=g' */index*
+sed -i -e 's=https://www.kerarqueo.com/=../../=g' */*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../=g' */*/*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../../=g' */*/*/*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../../../=g' */*/*/*/*/index*
+#shopping cart
+sed -i -e 's=span:before{content:"\\e07a";margin-right:10px;position:relative}=span:before{}=g' index* */index* */*/index* */*/*/index* */*/*/*/index* */*/*/*/*/index*
+sed -i -e 's=Catálago=Catálogo=g' index* */index* */*/index* */*/*/index* */*/*/*/index* */*/*/*/*/index*
+sed -i -e "s=.menu-item-has-children>a:first-child:after{font-family:'ETmodules';content:"'"3"'=".menu-item-has-children>a:first-child:after{font-family:'ETmodules';content:"'"﹀"=g' index*
 
