4a5a6c754c294b16a9436d295156adc27d22d210
Ruben Garcia <rubengarciahernandez@gmail.com>
2023-01-14 13:42:33 +0100

    Transform rest of index.html files

--- convert.sh
+++ convert.sh
@@ -1,1 +1,9 @@
 sed -i -e  's=https://www.kerarqueo.com/=./=g#' index*
+sed -i -e 's=https://www.kerarqueo.com/=../=g#' */index*
+sed -i -e 's=https://www.kerarqueo.com/=../../=g#' */*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../=g#' */*/*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../../=g#' */*/*/*/index*
+sed -i -e 's=https://www.kerarqueo.com/=../../../../../=g#' */*/*/*/*/index*
+
+
+
